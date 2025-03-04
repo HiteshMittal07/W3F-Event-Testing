e910e76a45214065261cf71249bd8ebcd98c9a93
HiteshMittal07 <smartyboy.hm15@gmail.com>
2025-03-05 00:15:46 +0530

    fix:minor changes

--- web3-functions/index.ts
+++ web3-functions/index.ts
@@ -3,40 +3,10 @@ import {
   Web3FunctionEventContext,
 } from "@gelatonetwork/web3-functions-sdk";
 import { ethers } from "ethers";
+
 const SIMPLE_COUNTER_ABI = [
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "newCounterValue",
-        type: "uint256",
-      },
-      {
-        indexed: false,
-        internalType: "address",
-        name: "msgSender",
-        type: "address",
-      },
-    ],
-    name: "IncrementCounter",
-    type: "event",
-  },
-  {
-    inputs: [],
-    name: "counter",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "increment",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
+  "function increment()",
+  "event IncrementCounter(uint256, address)",
 ];
 
 Web3Function.onRun(async (context: Web3FunctionEventContext) => {
